a042a77d493d3cc689e95898a781d1877e4700ef
Bastiaan Welmers <bastiaan@welmers.net>
2024-01-27 18:43:07 +0100

    Use unix socket instead of localhost tcp

--- tests/bast1aan/jira_reader/integration/test_jira.py
+++ tests/bast1aan/jira_reader/integration/test_jira.py
@@ -1,8 +1,12 @@
 import asyncio
 import json
+import os
 import unittest
+import socket
 
 import aiohttp.web
+
+import tempfile
 
 from tests.bast1aan.jira_reader.util import get_module_from_file, scriptdir
 
@@ -21,20 +25,27 @@ class TestRequestTicketHistory(unittest.IsolatedAsyncioTestCase):
                 body=open(scriptdir('test_jira/test_request_ticket_history/test_input.json'), 'r').read(),
                 headers={'content-type': 'application/json'},
             )
+
+        self.tmpdir = tempfile.mkdtemp()
+        self.socketpath = os.path.join(self.tmpdir, 'socket')
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self.sock.bind(self.socketpath)
+
         jira_app = aiohttp.web.Application()
         jira_app.add_routes([aiohttp.web.get('/rest/api/3/issue/ABC-123', jira)])
-        self.app_task = asyncio.create_task(aiohttp.web._run_app(jira_app, host='localhost', port=51391))
+        self.app_task = asyncio.create_task(aiohttp.web._run_app(jira_app, sock=self.sock))#  host='localhost', port=51391))
 
     async def asyncTearDown(self):
         self.app_task.cancel()
+        os.remove(self.socketpath)
+        os.rmdir(self.tmpdir)
         await super().asyncTearDown()
 
     async def test_test(self):
         expected = open(scriptdir('test_jira/test_request_ticket_history/test_input.json'), 'rb').read()
 
-        async with aiohttp.ClientSession() as client, \
-            client.get('http://localhost:51391/rest/api/3/issue/ABC-123?expand=renderedFields,changelog',
+        async with aiohttp.ClientSession(connector=aiohttp.UnixConnector(self.socketpath)) as client, \
+            client.get('https://jira-host/rest/api/3/issue/ABC-123?expand=renderedFields,changelog',
                        headers={'Accept': 'application/json'}) as response:
             result = await response.read()
             self.assertEqual(json.loads(result), json.loads(expected))
-
